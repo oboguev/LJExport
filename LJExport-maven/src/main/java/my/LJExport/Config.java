387c86ac26f8c65ab50d5a57297779b3e8625855
Sergey Oboguev <oboguev@yahoo.com>
2025-05-28 23:05:55 -0700

    maven migration

--- LJExport-maven/src/main/java/my/LJExport/Config.java
+++ LJExport-maven/src/main/java/my/LJExport/Config.java
@@ -31,9 +31,9 @@ public class Config
     public static String LoginUser = "oboguev";
 
     /* List of journals to download (comma or space-separated) */
-    public static String Users = "zhenziyou";
-    // public static String Users = "a_bugaev,tor85,oboguev,morky,krylov,rms1,pioneer_lj,holmogor,miguel_kud,colonelcassad,galkovsky,_devol_";
-
+    public static String Users = "oboguev";
+    // public static String Users = "eremei,afanarizm,jlm_taurus,corporatelie,wyradhe,nilsky_nikolay,von_hoffmann,a_samovarov,bantaputu,a_kaminsky,d_olshansky,rn_manifesto,ru_bezch,nep2,ego,hokma,laert,haritonov,1981dn,1981dn_dn,bantaputu,polit_ec,zhenziyou,a_bugaev,tor85,oboguev,morky,krylov,rms1,pioneer_lj,holmogor,miguel_kud,colonelcassad,galkovsky,_devol_";
+    
     /* Directory path to store downloaded files */
     public static String DownloadRoot = "R:";
     // public static String DownloadRoot = "F:\\@";

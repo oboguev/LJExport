206d21f3df1f4302de322204d647104dfb1482be
Sergey Oboguev <oboguev@yahoo.com>
2025-06-04 21:02:29 -0700

    misc

--- LJExport-maven/src/main/java/my/LJExport/readers/direct/PageReaderDirect.java
+++ LJExport-maven/src/main/java/my/LJExport/readers/direct/PageReaderDirect.java
@@ -66,12 +66,6 @@ public class PageReaderDirect implements PageReader, PageContentSource
             break;
 
         case "new-style":
-            if (Config.False)
-            {
-                Main.out("Skipping new-style " + parser.rurl);
-                return;
-                // ###
-            }
             parser = new PageParserDirectNewStyle(parser);
             break;
         }

be6901fa0f45010581d02d6207f2b95455a74612
Sergey Oboguev <oboguev@yahoo.com>
2025-07-01 09:56:48 -0700

    misc

--- LJExport-maven/src/main/java/my/LJExport/monthly/MainMakeMonthlyPages.java
+++ LJExport-maven/src/main/java/my/LJExport/monthly/MainMakeMonthlyPages.java
@@ -24,7 +24,8 @@ public class MainMakeMonthlyPages
     // private static String Users = "alex_vergin,asriyan,blog_10101,hokma,sergeytsvetkov";
     // private static String Users = "alex_vergin";
     // private static final String Users = ALL_USERS;
-    private static String Users = "amfora,colonelcassad,fluffyduck2,genby,kot_begemott,lasido,sergeytsvetkov,von_hoffmann";
+    //private static String Users = "amfora,colonelcassad,fluffyduck2,genby,kot_begemott,lasido,sergeytsvetkov,von_hoffmann";
+    private static String Users = "hurtmann,maxim_sokolov,obsrvr,ru_nationalism,schegloff";
 
     public static void main(String[] args)
     {

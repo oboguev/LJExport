d324c06d54312514783b6d1fd8162c7b5ef34a6d
I800461 <sergey.oboguev@sap.com>
2025-07-15 15:11:26 -0700

    misc

--- LJExport-maven/src/main/java/my/LJExport/monthly/MonthProcessor.java
+++ LJExport-maven/src/main/java/my/LJExport/monthly/MonthProcessor.java
@@ -22,7 +22,8 @@ public class MonthProcessor
     private final String whichDir;
     private final boolean ljsearch;
 
-    public MonthProcessor(String pagesMonthDir, List<String> pageFileNames, String monthlyFilePrefix, String year, String month, String whichDir, boolean ljsearch)
+    public MonthProcessor(String pagesMonthDir, List<String> pageFileNames, String monthlyFilePrefix, String year, String month,
+            String whichDir, boolean ljsearch)
     {
         this.pagesMonthDir = pagesMonthDir;
         this.pageFileNames = pageFileNames;
@@ -32,69 +33,83 @@ public class MonthProcessor
         this.whichDir = whichDir;
         this.ljsearch = ljsearch;
     }
-    
+
     public void process() throws Exception
     {
         MonthCollectors mcs = new MonthCollectors(year, month, ljsearch);
-        
+
         for (String fn : pageFileNames)
         {
             String pageFileFullPath = pagesMonthDir + File.separator + fn;
             
             try
             {
-                if (Config.False && pageFileFullPath.equals("F:\\WINAPPS\\LJExport\\journals\\harmfulgrumpy\\pages\\2016\\01\\288850.html"))
-                {
-                    Util.noop();
-                }
-                
-                // Main.out("    " + pageFileFullPath);
-                
-                String rid = Util.stripTail(fn, ".html");
-                int rid_numeric = Integer.parseInt(rid);
-                
                 PageParserDirectBase parser = new PageParserDirectBasePassive();
                 parser.rurl = Util.extractFileName(pageFileFullPath);
-
                 parser.pageSource = Util.readFileAsString(pageFileFullPath);
                 parser.parseHtml(parser.pageSource);
-                
-                if (ljsearch)
-                {
-                    // do nothing
-                }
-                else
-                {
-                    switch (parser.detectPageStyle())
-                    {
-                    case "classic":
-                        parser = new PageParserDirectClassic(parser);
-                        parser.deleteDivThreeposts();
-                        break;
 
-                    case "new-style":
-                        parser = new PageParserDirectNewStyle(parser);
-                        parser.deleteDivThreeposts();
-                        break;
-                        
-                    case "rossia.org":
-                        parser = new PageParserDirectRossiaOrg(parser);                        
-                        break;
-                        
-                    case "dreamwidth.org":
-                        parser = new PageParserDirectDreamwidthOrg(parser);                        
-                        break;
-                    }
-                }
-
-                mcs.addPage(parser, rid_numeric, whichDir);
+                processOneFile(mcs, pageFileFullPath, fn, parser);
             }
             catch (Exception ex)
             {
                 throw new Exception("Error processing file " + pageFileFullPath, ex);
             }
         }
-        
+
         mcs.complete(monthlyFilePrefix);
+    }
+
+    private void processOneFile(MonthCollectors mcs, String pageFileFullPath, String fn, PageParserDirectBase parser)
+            throws Exception
+    {
+        try
+        {
+            if (Config.False
+                    && pageFileFullPath.equals("F:\\WINAPPS\\LJExport\\journals\\harmfulgrumpy\\pages\\2016\\01\\288850.html"))
+            {
+                Util.noop();
+            }
+
+            // Main.out("    " + pageFileFullPath);
+
+            String rid = Util.stripTail(fn, ".html");
+            int rid_numeric = Integer.parseInt(rid);
+
+            if (ljsearch)
+            {
+                // do nothing
+            }
+            else
+            {
+                switch (parser.detectPageStyle())
+                {
+                case "classic":
+                    parser = new PageParserDirectClassic(parser);
+                    parser.deleteDivThreeposts();
+                    break;
+
+                case "new-style":
+                    parser = new PageParserDirectNewStyle(parser);
+                    parser.deleteDivThreeposts();
+                    break;
+
+                case "rossia.org":
+                    parser = new PageParserDirectRossiaOrg(parser);
+                    break;
+
+                case "dreamwidth.org":
+                    parser = new PageParserDirectDreamwidthOrg(parser);
+                    break;
+                }
+            }
+
+            mcs.addPage(parser, rid_numeric, whichDir);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error processing file " + pageFileFullPath, ex);
+        }
+
     }
 }

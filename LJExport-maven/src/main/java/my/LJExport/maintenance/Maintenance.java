88f735aabefec1afe58259a7c855d1613ff60a79
Sergey Oboguev <oboguev@yahoo.com>
2025-07-24 04:00:53 -0700

    misc

--- LJExport-maven/src/main/java/my/LJExport/maintenance/Maintenance.java
+++ LJExport-maven/src/main/java/my/LJExport/maintenance/Maintenance.java
@@ -39,8 +39,8 @@ public class Maintenance
     // private static final String Users = "a_kaminsky";
     // private static final String Users = "1981dn,1981dn_dn,64vlad,_devol_";
     // private static final String Users = "oboguev";
-    // private static final String Users = "harmfulgrumpy.dreamwidth-org";
-    // private static final String Users = "udod99.lj-rossia-org";
+    // private static final String Users = "nationalism.org";
+    // private static final String Users = "harmfulgrumpy.dreamwidth-org,udod99.lj-rossia-org";
 
     private static int ParallelismDefault = 20;
     private static int ParallelismMonthly = 5;
@@ -100,7 +100,7 @@ public class Maintenance
             // do_users(Users, FixDirectoryLinks.class);
             // do_users(Users, FixLongPaths.class);
             do_users(Users, DetectFailedDownloads.class);
-            // then run MainRedownloadFailedLinks
+            // --- then run MainRedownloadFailedLinks
             // do_users(Users, FixFileExtensions.class);
 
             txLog.writeLineSafe("");

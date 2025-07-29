227032f76a4bf5c49c72cd68eb2b40bbe9985a3f
I800461 <sergey.oboguev@sap.com>
2025-07-29 14:19:20 -0700

    misc

--- LJExport-maven/src/main/java/my/LJExport/runtime/browsers/FirefoxUserAgent.java
+++ LJExport-maven/src/main/java/my/LJExport/runtime/browsers/FirefoxUserAgent.java
@@ -8,24 +8,31 @@ import org.openqa.selenium.JavascriptExecutor;
 
 public class FirefoxUserAgent
 {
-    public static String getUserAgent()
+    private static String resolvedUserAgent;
+
+    public static synchronized String getUserAgent()
     {
-        System.setProperty("webdriver.gecko.driver", GeckoDriverFinder.findGeckoDriverInPath());
-
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("-headless");
-
-        WebDriver driver = new FirefoxDriver(options);
-        try
+        if (resolvedUserAgent == null)
         {
-            driver.get("about:blank");
-            String userAgent = (String) ((JavascriptExecutor) driver)
-                    .executeScript("return navigator.userAgent;");
-            return userAgent;
+            System.setProperty("webdriver.gecko.driver", GeckoDriverFinder.findGeckoDriverInPath());
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("-headless");
+
+            WebDriver driver = new FirefoxDriver(options);
+            try
+            {
+                driver.get("about:blank");
+                String userAgent = (String) ((JavascriptExecutor) driver)
+                        .executeScript("return navigator.userAgent;");
+                resolvedUserAgent = userAgent;
+            }
+            finally
+            {
+                driver.quit();
+            }
         }
-        finally
-        {
-            driver.quit();
-        }
+
+        return resolvedUserAgent;
     }
 }

112ed1187f845ba8d09a0b0df0c3b6416e3bbc6a
I800461 <sergey.oboguev@sap.com>
2025-07-31 17:00:54 -0700

    misc

--- LJExport-maven/src/main/java/my/LJExport/runtime/lj/Sites.java
+++ LJExport-maven/src/main/java/my/LJExport/runtime/lj/Sites.java
@@ -1,8 +1,35 @@
 package my.LJExport.runtime.lj;
 
+import java.net.URL;
+
 public class Sites
 {
     public static final String DreamwidthOrg = "dreamwidth.org";
     public static final String Livejournal = "livejournal.com";
     public static final String RossiaOrg = "lj.rossia.org";
+    public static final String ArchiveOrg = "archive.org";
+    public static final String Other = "--other--";
+
+    private static final String[] all = { Livejournal, DreamwidthOrg, RossiaOrg, ArchiveOrg };
+
+    public static String which(String url) throws Exception
+    {
+        URL xurl = new URL(url);
+        String host = xurl.getHost();
+        if (host != null)
+            host = host.trim().toLowerCase();
+        if (host == null || host.length() == 0)
+            throw new Exception("Missing host name in URL " + url);
+        
+        if (host.equals("livejournal.net") || host.endsWith(".livejournal.net"))
+            return Livejournal;
+
+        for (String site : all)
+        {
+            if (host.equals(site) || host.endsWith("." + site))
+                return site;
+        }
+        
+        return Other;
+    }
 }
